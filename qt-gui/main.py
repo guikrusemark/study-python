ba95f497735c36fc2654f863c03e1d8b618707da
guikrusemark <guilhermekrusemark@gmail.com>
2023-01-08 22:09:30 -0300

    curso 2

---
+++ qt-gui/main.py
@@ -0,0 +1,12 @@
+import sys
+import os
+from PyQt5.QtGui import QGuiApplication
+from PyQt5 import QQmlApplicationEngine
+from PyQt5.QtQuick import QQuickWindow
+
+QQuickWindow.setSceneGraphBackend('software')
+app = QGuiApplication(sys.argv)
+engine = QQmlApplicationEngine()
+engine.quit.connect(app.quit)
+engine.load('./UI/main.qml')
+sys.exit(app.exec())
